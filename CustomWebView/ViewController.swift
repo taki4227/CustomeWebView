d4f2922f15854a793bf181b93456b040cd054fc3
taki4227 <naoki.takimoto@yourmystar.jp>
2018-10-26 11:03:53 +0900

    Initial Commit

---
+++ CustomWebView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomWebView
+//
+//  Created by 滝本直樹 on 2018/10/26.
+//  Copyright © 2018年 taki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
